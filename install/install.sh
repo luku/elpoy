c3aa6d5f65ff4d86f42c15fa34e259267d9e68c1
Luk Puk <lp1051@gmail.com>
2014-09-09 13:23:00 +0200

    try the whole thing

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# need to set the terminal into something... "dumb" is used if current $TERM is not found, "xterm" is used with ordinary SSH session...
+export TERM=dumb
 
 # set environmental variables that we will need in child script/processes
 #export DEV_MODE=$1
@@ -15,9 +17,10 @@ BRANCH_NAME="$BRANCH_NAME"
 # target dir for git repo
 TARGET_DIR="$TARGET_DIR"
 
-#DIR=$( cd "$( dirname "$0" )" && pwd )
+# INSTALL_DIR=$( cd "$( dirname "$0" )" && pwd )
+INSTALL_DIR="$( dirname "$0" )"
 echo "deploy action = $DEPLOY_ACTION and DEV_MODE = $DEV_MODE and TARGET_DIR = $TARGET_DIR"
-echo "pwd = $( pwd ) "
+echo "pwd = $( pwd ) INSTALL_DIR = $INSTALL_DIR"
 
 # must change the current directory for git to work on
 cd "$TARGET_DIR"
@@ -28,29 +31,27 @@ remote_name=$(git remote show);
 #repo_url=$(git ls-remote --get-url "$remote_name");
 
 echo "pwd = $( pwd ) remote_name = $remote_name and repo_url = $repo_url"
-echo $(pidof -x monitor_dropbox.sh);
 
-php install/install.php
-exit;
 
-if [ "$DEPLOY_ACTION" = "install" ] 
-then
+if [ "$DEPLOY_ACTION" = "install" ]; then
 	# on repo clone we must execute the PHP install manually
-	php install/install.php "$DEV_MODE" "$DEPLOY_ACTION"
-	# echo 'Starting monitoring scripts'
+	php "$INSTALL_DIR"/install.php "$DEPLOY_ACTION" "$DEV_MODE" 
+	echo 'Starting monitoring scripts'
 	# this will start the monitoring scripts, but admin still needs to update crontab!!
-	# sh cron1.sh > /dev/null 2>&1
+	sh cron1.sh > /dev/null 2>&1
 else 
 	echo "Pulling from $remote_name/$BRANCH_NAME"
 	# should I use origin or $remote_name here??? git pull -s recursive -X theirs "$remote_name"/"$branch_name"
 	git pull -s recursive -X theirs "$remote_name"/"$BRANCH_NAME"
 	# if config or monitoring scripts/templates were changed run installation update 
 	changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
-	echo "$changed_files" | grep 'config\.php\|\.sh\.tpl' && { 
-		php install/install.php "$DEV_MODE" "$DEPLOY_ACTION"
+	echo "$changed_files" | grep 'config\.php\|\.sh' && { 
+		php "$INSTALL_DIR"/install.php "$DEPLOY_ACTION" "$DEV_MODE" 
 		# on config change we have to update monitoring scripts, so kill them!!!
 		# kill -9 $(pidof -x monitor_dropbox.sh)
 		# kill -9 $(pidof -x monitor_in2.sh)
 		# kill -9 $(pidof -x outfolder_monitor.sh)
 	}
 fi
+
+exit 0
